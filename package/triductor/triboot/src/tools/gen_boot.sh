aa1f434dc5295991c8a31bfe118e8b75da091891
moeee323 <mr_moritz323@hotmail.com>
2025-02-18 22:30:10 +0100

    added all packages usefull for modem configuration to .config, fixed build script as shown for non mini version https://github.com/BPI-SINOVOIP/THG6500-TAX2-OPENWRT-BSP/pull/10

--- package/triductor/triboot/src/tools/gen_boot.sh
+++ package/triductor/triboot/src/tools/gen_boot.sh
@@ -10,6 +10,7 @@ dd if=/dev/zero ibs=1k count=200 | tr "\000" "\377" > fileFF.tmp;
 
 cat TR6560-bootimage_nodtb.bin fileFF.tmp  | head --bytes=$BOOT_FAC_LEN > TR6560-bootimage.bin.tmp;
 cat TR6560-bootimage.bin.tmp boot.dtb   > TR6560-bootimage.bin;
+mkdir -p ../bin
 mv TR6560-bootimage.bin ../bin/;
 
 rm -f  *.tmp;
